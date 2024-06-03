bdb9257e01f07c17c2d226e7dbb4c830781ddf35
Flavio <flabrito43@gmail.com>
2024-06-02 21:24:49 -0300

    fin sprint 1

--- src/controllers/users/signin.controller.js
+++ src/controllers/users/signin.controller.js
@@ -1,26 +1,36 @@
-import { resolveSoa } from 'dns'
 import User from '../../models/users.model.js'
 import bycrypt from 'bcrypt'
 
 
 export let signin = async(req, res, next) => {
 
-  const email = req.body.email
-  const userPass = req.body.password
+  const data = {
+    email: req.body.email,
+    password:req.body.password
+  }
   
   try {
 
-    const users = await User.findOne({email:email})
+    const user = await User.findOne({email: data.email})
 
-    if(bycrypt.compareSync(userPass, users.password)){
-      return res.status(201).json({
-        success:true,
-        msg:'Password correcto'
+      if(!user){
+        return res.status(401).json({
+          msg: 'Usuario no encontrado'
+        })
+      }
+      
+      if(bycrypt.compareSync(data.password, user.password)){
+        return res.status(201).json({
+          success:true,
+          msg:'Password correcto'
       })
-    }else{
-      return res.status(400).json({msg:'Password failed'})
-    }
-
+      }else{
+       return res.status(400).json({
+        msg:'Password incorrecta'
+      })
+      }
+      
+    
   }catch (error) {
     console.log(error)
     return res.status(500).json({
--- src/routes/events.route.js
+++ src/routes/events.route.js
@@ -2,11 +2,13 @@ import {Router} from 'express'
 import { newEvent } from '../controllers/events/eventCreated.controller.js'
 import { events } from '../controllers/events/events.controller.js'
 import { eventId } from '../controllers/events/eventId.controller.js'
+import { attendes } from '../controllers/events/attendes.controller.js'
 const router = Router()
 
 router.get('/', events)
 router.get('/:id', eventId)
 router.post('/newEvent', newEvent)
+router.post('/attendes', attendes)
 
 
 export default  router
